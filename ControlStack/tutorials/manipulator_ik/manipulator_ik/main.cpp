3b765fe885b8d6eeaf97b4314e0c4a297a796e7d
pangfumin <pangfumin@gmail.com>
2023-12-05 15:58:46 +0800

    add some sample

--- ControlStack/tutorials/manipulator_ik/manipulator_ik/main.cpp
+++ ControlStack/tutorials/manipulator_ik/manipulator_ik/main.cpp
@@ -1,16 +1,15 @@
 
 
 #include<stdbool.h> //for bool
-//#include<unistd.h> //for usleep
-//#include <math.h>
 
 #include "mujoco.h"
-#include "glfw3.h"
+#include "GLFW/glfw3.h"
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
+#include <string>
 
-#include "constrained.c"
+#include "nlopt.h"
 
 
 
@@ -25,15 +24,13 @@ double xact, zact;
 double a = 0.25;
 const double omega = 0.4;
 double center_x=1, center_z=1.25;
-// char xmlpath[] = "../myproject/template_writeData/pendulum.xml";
-// char datapath[] = "../myproject/template_writeData/data.csv";
 
 //simulation end time
 double simend = 2*3.14/omega + 0.25;
 
 //Change the path <template_writeData>
 //Change the xml file
-char path[] = "../myproject/manipulator_ik/";
+std::string path = SOURCE_CODE_PATH + std::string("/tutorials/manipulator_ik/manipulator_ik/");
 char xmlfile[] = "manipulator.xml";
 
 
@@ -64,6 +61,167 @@ mjtNum previous_time = 0;
 float_t ctrl_update_freq = 100;
 mjtNum last_update = 0.0;
 mjtNum ctrl;
+
+
+double x_target, z_target;
+
+/*Optimization problem
+
+Cost = 0;
+
+equality constraints:
+  ceq_1 = x_target - sensordata[0];
+  ceq_2 = z_target - sensordata[2];
+
+decision variables
+  Xin = {q_1, q_2}; joint angles
+
+bounds
+  q_1, q_2 = {-3.14, 3.14}
+
+*/
+
+
+/******************************/
+void simulator(const double Xin[2],double Xout[2])
+{
+  dsim->qpos[0] = Xin[0]; dsim->qpos[1] = Xin[1];
+  dsim->ctrl[0] = dsim->qpos[0]; dsim->ctrl[2] = dsim->qpos[1];
+  mj_forward(m,dsim);
+  Xout[0] = dsim->sensordata[0];
+  Xout[1] = dsim->sensordata[2];
+  // printf("tip: %f %f \n",d->sensordata[0], d->sensordata[2]);
+}
+//**************************
+
+// typedef struct {
+//     double a[5];
+// }mycost_data;
+//
+// typedef struct {
+//     double ceq_1;
+//     double ceq_2;
+// }myequalityconstraints_data;
+//
+// typedef struct {
+//     double cin_1;
+// }myinequalityconstraints_data;
+
+
+double mycost(unsigned n, const double *x, double *grad, void *costdata)
+{
+    // mycost_data *data = (mycost_data *) costdata;
+    //
+    // int i;
+    // double a[5]={0};
+    // for (i=0;i<n;i++)
+    //   a[i] = data->a[i];
+
+    double cost = 0;
+    // for (i=0;i<n;i++)
+    //   cost += a[i]*x[i]*x[i];
+
+    return cost;
+}
+
+
+void myequalityconstraints(unsigned m, double *result, unsigned n,
+                             const double *x,  double *grad,
+                             void *equalitydata)
+{
+    // myequalityconstraints_data *data = (myequalityconstraints_data *) equalitydata;
+    //
+    // double c1 = data->ceq_1;
+    // double c2 = data->ceq_2;
+    // double x1 = x[0], x2 = x[1], x3 = x[2], x4 = x[3], x5 = x[4];
+    // result[0] = x1+x2+x3-c1; //5;
+    // result[1] = x3*x3+x4-c2; //2;
+
+
+    // equality constraints:
+    //   ceq_1 = x_target - sensordata[0];
+    //   ceq_2 = z_target - sensordata[2];
+    double Xout[2]={0};
+    simulator(x,Xout);
+
+    result[0] = x_target - Xout[0]; //d->sensordata[0];
+    result[1] = z_target - Xout[1]; //d->sensordata[2];
+ }
+
+ // double myinequalityconstraints(unsigned m, double *result, unsigned n,
+ //                                const double *x,  double *grad,
+ //                                void* inequalitydata)
+ // {
+ //     myinequalityconstraints_data *data = (myinequalityconstraints_data *) inequalitydata;
+ //
+ //     double c1 = data->cin_1;
+ //     double x1 = x[0], x2 = x[1], x3 = x[2], x4 = x[3], x5 = x[4];
+ //     result[0] = x4*x4+x5*x5-c1; //5;
+ //  }
+
+
+void inverse_kinematics(double Xin[2], double Xref[2])
+{
+int i;
+nlopt_opt opt;
+
+x_target = Xref[0];
+z_target = Xref[1];
+
+//double Xin[2]={-0.5,1}; //joint angle
+//double Xout[2] = {0}; //tip position
+//simulator(Xin,Xout);
+//printf("tip: %f %f \n",Xout[0], Xout[1]);
+
+//establish sizes
+unsigned n = 2; //number of decision variables
+unsigned m_eq = 2; //number of equality constraints
+//unsigned m_in = 1; //number of inequality constraints
+
+//bounds for decision variables
+double lb[] = { -3.14, -3.14 }; /* lower bounds */
+double ub[] = { 3.14, 3.14 }; /* lower bounds */
+
+//Set the algorithm and dimensionality
+//L,G = global/local
+//D,N = derivative / no derivative
+opt = nlopt_create(NLOPT_LN_COBYLA, n); /* algorithm and dimensionality */
+
+//Set the lower and upper bounds
+nlopt_set_lower_bounds(opt, lb);
+nlopt_set_upper_bounds(opt, ub);
+
+//Set up cost
+// mycost_data costdata;
+// for (i=0;i<n;i++)
+//   costdata.a[i]=1;
+nlopt_set_min_objective(opt, mycost, NULL);
+
+//set up equality constraint
+double tol_eq[]={1e-8,1e-8};
+// myequalityconstraints_data equalitydata;
+// equalitydata.ceq_1 = 5;
+// equalitydata.ceq_2 = 2;
+nlopt_add_equality_mconstraint(opt, m_eq, myequalityconstraints, NULL, tol_eq);
+
+// double tol_in[]={1e-8};
+// myinequalityconstraints_data inequalitydata;
+// inequalitydata.cin_1 = 5;
+// nlopt_add_inequality_mconstraint(opt, m_in, myinequalityconstraints,&inequalitydata, tol_in);
+
+
+nlopt_set_xtol_rel(opt, 1e-4);
+//double x[] = { 1, 1, 1, 2, 1 };  // initial guess
+double minf; /* `*`the` `minimum` `objective` `value,` `upon` `return`*` */
+if (nlopt_optimize(opt, Xin, &minf) < 0) {
+    printf("nlopt failed!\n");
+}
+else {
+    printf("found minimum at f(%g,%g) = %0.10g\n", Xin[0], Xin[1],minf);
+}
+
+nlopt_destroy(opt);
+}
 
 // keyboard callback
 void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods)
@@ -263,16 +421,13 @@ void my_controller(const mjModel* m, mjData* d)
 int main(int argc, const char** argv)
 {
 
-    // activate software
-    mj_activate("mjkey.txt");
-
     char xmlpath[100]={};
     char datapath[100]={};
 
-    strcat(xmlpath,path);
+    strcat(xmlpath,path.c_str());
     strcat(xmlpath,xmlfile);
 
-    strcat(datapath,path);
+    strcat(datapath,path.c_str());
     strcat(datapath,datafile);
 
 
@@ -379,7 +534,6 @@ int main(int argc, const char** argv)
     mj_deleteData(d);
     mj_deleteData(dsim);
     mj_deleteModel(m);
-    mj_deactivate();
 
     // terminate GLFW (crashes with Linux NVidia drivers)
     #if defined(__APPLE__) || defined(_WIN32)
